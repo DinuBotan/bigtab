1958aa4b5c7f5c0be888d1ed13618cfe06f9f4f3
a-n-u-b-i-s <watalanubhava@gmail.com>
2022-04-11 11:03:55 -0400

    build: updates to eslint config

--- src/pages/popup/App.tsx
+++ src/pages/popup/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const App = (): JSX.Element => {
-  return (
-    <div>
-      <h1>Popup Page</h1>
-      <p>If you are seeing this, React is working!</p>
-    </div>
-  );
-};
+const App = (): JSX.Element => (
+  <div>
+    <h1>Popup Page</h1>
+    <p>If you are seeing this, React is working!</p>
+  </div>
+);
 
 export default App;
